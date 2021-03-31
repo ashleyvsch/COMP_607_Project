2fb37314b2c11bd2d1c622f271052c21e522c2e3
Ashley Schwartz <ashleyschwartz@ashleys-mbp.attlocal.net>
2021-03-31 16:43:44 -0700

    3/31 updates

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 <ul>
   <li>
-    <a href="create.php"><strong>Create</strong></a> - add a user
+    <a href="create.php"><strong>Create</strong></a> - add a dog
   </li>
   <li>
-    <a href="read.php"><strong>Read</strong></a> - find a user
+    <a href="read.php"><strong>Read</strong></a> - find a dog
   </li>
-  <li><a href="update.php"><strong>Update</strong></a> - edit a user</li>
+  <li><a href="update.php"><strong>Update</strong></a> - edit a dog</li>
 </ul>
 
 <?php include "templates/footer.php"; ?>
