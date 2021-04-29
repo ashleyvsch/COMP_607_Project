ddc92382b458b7a812e72f1cc8682a9273799fe9
Ashley Schwartz <ashleyschwartz@ashleys-mbp.attlocal.net>
2021-04-29 10:40:39 -0700

    updates

--- public/templates/footer.php
+++ public/templates/footer.php
@@ -1,2 +1,7 @@
 </body>
+<br>
+<br>
+<h6 class = "center" >github: ashleyvsch/COMP_607_Project</h6>
+<h6 class = "center" ><a href="https://github.com/ashleyvsch/COMP_607_Project"> click for github</a></h6>
+<br>
 </html>
